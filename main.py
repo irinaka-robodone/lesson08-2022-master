b332d4e011e71a23817fa2ca382e9fa1fe3dfdcf
Irinaka Robodone <irinaka.robodone@gmail.com>
2022-07-21 20:15:37 +0900

    init commit by ki

--- main.py
+++ main.py
@@ -1,30 +1,23 @@
 from microbit import *
+#def baa(a,b):
+    #display.set_pixel(a,4,9) 
+    #display.set_pixel(b,4,9)
+BAA = Image("00000:""00000:""00000:""00000:""09900")
 
-while not button_a.was_pressed():
-    pass
-display.scroll('Are You Ready?')
+while not if button_a.was_pressed() or button_b.was_pressed():
+    display.scroll("BOTAN　OSHITE")
+display.set_pixer(3,3,9)
+display.show(BAA)
+sleep(1000)
 
-while not button_a.was_pressed():
-    pass
-display.scroll('Game Start!')
-
-def display_bar(bar_pos):
-    for pos in bar_pos:
-        display.set_pixel(pos, 4, 9)
-    # display.clear()
-
-def display_ball(ball_pos):
-    display.set_pixel(ball_pos[0], ball_pos[1], 9)
-
-dt = 10
-bar_pos = [2, 3]
-ball_pos = [3, 0]
-IsGameOver = False
-
-while not IsGameOver:
+while True:
+    display.show(BAA)
+    if button_a.was_pressed():
+        display.show(BAA.shift_left(1))
+    elif button_b.was_pressed():
+        display.show(BAA.shift_right(1))
     
-    display_bar(bar_pos)
-    display_ball(ball_pos)
     
-    sleep(10)
-    display.clear()
+    
+    
+    

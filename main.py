d04d59c069b6d9e65c91387ef81f469066c1db81
Irinaka Robodone <irinaka.robodone@gmail.com>
2022-07-21 20:20:51 +0900

    init commit by ka

--- main.py
+++ main.py
@@ -1,30 +1,9 @@
 from microbit import *
+display.scroll("Hello, World!")
 
 while not button_a.was_pressed():
     pass
-display.scroll('Are You Ready?')
+display.scroll('Go!')
 
-while not button_a.was_pressed():
-    pass
-display.scroll('Game Start!')
 
-def display_bar(bar_pos):
-    for pos in bar_pos:
-        display.set_pixel(pos, 4, 9)
-    # display.clear()
-
-def display_ball(ball_pos):
-    display.set_pixel(ball_pos[0], ball_pos[1], 9)
-
-dt = 10
-bar_pos = [2, 3]
-ball_pos = [3, 0]
-IsGameOver = False
-
-while not IsGameOver:
-    
-    display_bar(bar_pos)
-    display_ball(ball_pos)
-    
-    sleep(10)
-    display.clear()
+ber =
